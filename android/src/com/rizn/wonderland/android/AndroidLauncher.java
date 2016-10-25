c25337ae3a8ad0d2f34110a907d5ed1da7c55f1e
richardpiotrowski <richard.piotrowski@capablue.com>
2016-10-25 18:59:00 +0200

    cleanup, extra screens

--- android/src/com/rizn/wonderland/android/AndroidLauncher.java
+++ android/src/com/rizn/wonderland/android/AndroidLauncher.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.rizn.wonderland.Wonderland;
-import com.rizn.wonderland.Wonderland;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
